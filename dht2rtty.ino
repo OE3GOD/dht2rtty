1a2ed46a7f566bad20ab86aa62de8a5ff40d9edf
OE3GOD <oe3god@gmail.com>
2017-12-31 00:06:19 +0100

    Codepage Change

--- dht2rtty.ino
+++ dht2rtty.ino
@@ -27,19 +27,19 @@ Tone tone1;
 #define mark 2295
 #define space 2125
  
-#define is_lowercase(ch)    ((c) >= 'a' && (c) <= 'z' || (c) == ' ')
-#define is_uppercase(ch)    ((c) >= 'A' && (c) <= 'Z' || (c) == ' ')
+//#define is_lowercase(ch)    ((c) >= 'a' && (c) <= 'z' || (c) == ' ')
+//#define is_uppercase(ch)    ((c) >= 'A' && (c) <= 'Z' || (c) == ' ')
  
 char letters_arr[32] = "xEnA SIUrDRJNFCKTZLWHYPQOBGxMXV";
 char figures_arr[32] = "x3n- '87rw4b,x:(5+)2x6019?xx./=";
 char outstr[20]; 
 
-char current_mode;
+int current_mode;
  
 void setup(){
   
   dht.begin();
-  
+  //Serial.begin(9600);
   tone1.begin(Audiopin);
 
   lcd.begin();
@@ -49,7 +49,7 @@ void setup(){
   lcd.setCursor(0, 1);
   lcd.print("OE3GOD-Dez. 2017");  
   
-  delay(2500);
+  delay(2000);
 }
  
 void loop(){
@@ -69,8 +69,9 @@ void loop(){
     
     // Auf Buchstaben umstellen, sonst erscheint 46 statt RY !
     rtty_txbyte(LETTERS_SHIFT);
-    current_mode = 'L';
-    delay(1000); 
+    Serial.println("Letter Shift");
+    current_mode = 0;
+    delay(100); 
     
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -82,7 +83,7 @@ void loop(){
     lcd.print(t);
     lcd.print(" *C");
     
-    rtty_txstring("RYRYRYrn");
+    rtty_txstring("rnRYRYRYrn");
     rtty_txstring("ARDUINO SENSOR2RTTY DE OE3GODrn");
     dtostrf(t,7,1,outstr);
     rtty_txstring("TEMPERATURE: ");
@@ -94,10 +95,11 @@ void loop(){
     rtty_txstring("HUMIDITY: ");
     rtty_txstring(outstr);
     rtty_txstring(" PERCENT");
-    rtty_txstring("rn");
- 
+    rtty_txstring("rn"); 
+    //rtty_txstring("OE3GOD OE3GODrn");
     delay(1000);
     tone1.stop();    
+    delay(1000);
     
    }  
    
@@ -106,13 +108,12 @@ void loop(){
  
  void rtty_txstring(String txstr)
 {
-
   char c;
   uint8_t b;
   int l,i;
 
-  rtty_txbyte(LETTERS_SHIFT);
-  current_mode = 'L';
+  //rtty_txbyte(LETTERS_SHIFT);
+  //current_mode = "L";
   
   l = txstr.length();
   
@@ -123,38 +124,56 @@ void loop(){
     if (c == 'n')
     {
       rtty_txbyte(LF);
+      //Serial.println();
     }
     else if (c == 'r')
     {
       rtty_txbyte(CR);
     }
-    else if (is_lowercase(txstr) || is_uppercase(txstr))
+    else if (c == 'b')
     {
-       // if (is_lowercase(txstr))
-       // {
-        //  c -= 32;
-       // }
+      rtty_txbyte(NUMBERS_SHIFT);
+      //Serial.println("Number Shift");
+      current_mode = 1;
+      rtty_txbyte(bell);
+    }
+    else 
+    {
+      b = 0;
+      b = char_to_baudot(c, letters_arr);
  
-      if (current_mode != 'L')
+      if (b != 0)
+      {
+        if (current_mode != 0)
       {
         rtty_txbyte(LETTERS_SHIFT);
-        current_mode = 'L';
+        
+      //Serial.println("Letter Shift");
+        current_mode = 0;
       }
- 
-      rtty_txbyte(char_to_baudot(c, letters_arr));
-    }
-    else
-    {
+        rtty_txbyte(b);        
+        //Serial.print(b);
+      //  Serial.print(c);
+      } else 
+      {
+      b = 0;
       b = char_to_baudot(c, figures_arr);
  
-      if (b != 0 && current_mode != 'N')
+      if (b != 0)
+      {
+        if (current_mode != 1)
       {
         rtty_txbyte(NUMBERS_SHIFT);
-        current_mode == 'N';
+        
+    //  Serial.println("Number Shift");
+        current_mode = 1;
       }
- 
-      rtty_txbyte(b);
-    }
+        rtty_txbyte(b);  
+        Serial.print(c);      
+      }
+      }
+
+   }
     l--;
 //    txstr++;
   }
@@ -165,7 +184,7 @@ void loop(){
  
 uint8_t char_to_baudot(char c, char *array)
 {
-  int i;
+  int i=0;
   //Serial.println(c);
   for (i = 1; i < 32; i++)
   {
@@ -207,12 +226,12 @@ void rtty_txbyte(uint8_t b)
 void rtty_txbit (int bit) {
   if (bit) {
     // High - mark
-    tone1.play(mark);    
-  }
+    tone1.play(mark);  
+      }
   else {
     // Low - space
     tone1.play(space);
-  }
+      }
   // 1/0,022 = 45,45 baud.
   // 1/0,020 = 50 baud.
   delay(22);
